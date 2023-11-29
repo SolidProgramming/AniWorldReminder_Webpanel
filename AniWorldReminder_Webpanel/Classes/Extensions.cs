424c248be99934ddc5523dae8ee181f0d7394de8
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2023-11-29 13:27:31 +0100

    [+] Const Routes [+] UrlSanitize Extension Method [+] Series Detail Page

---
+++ AniWorldReminder_Webpanel/Classes/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AniWorldReminder_Webpanel.Classes
+{
+    public static class Extensions
+    {
+        public static string UrlSanitize(this string text)
+        {
+            return text.Replace(' ', '-')
+                .Replace(":", "")
+                .Replace("~", "")
+                .Replace("'", "")
+                .Replace(",", "")
+                .Replace("’", "");
+        }
+    }
+}
---
+++ AniWorldReminder_Webpanel/Classes/Routes.cs
@@ -0,0 +1,12 @@
+﻿namespace AniWorldReminder_Webpanel.Classes
+{
+    public static class Routes
+    {
+        public const string Search = "/search";
+        public const string Index = "/";
+        public const string Login = "/login";
+        public const string Logout = "/logout";
+        public const string Verify = "/verify";
+        public const string SeriesDetail = "/series";
+    }
+}
