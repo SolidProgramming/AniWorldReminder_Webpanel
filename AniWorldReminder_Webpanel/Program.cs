88f5d3da8540e44e0448f97dd9b53ff2c1aca848
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2023-11-29 09:21:48 +0100

    [+] HSTS

--- AniWorldReminder_Webpanel/Program.cs
+++ AniWorldReminder_Webpanel/Program.cs
@@ -5,9 +5,14 @@ global using AniWorldReminder_Webpanel.Models;
 global using AniWorldReminder_Webpanel.Interfaces;
 global using Newtonsoft.Json;
 using Havit.Blazor.Components.Web;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHsts(_ =>
+{
+    _.Preload = true;
+    _.IncludeSubDomains = true;
+});
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
@@ -32,12 +37,7 @@ builder.Services.AddScoped(_ =>
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
