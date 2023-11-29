8f67fee7926ec02b659d04cdac2cf719fb2d10bd
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2023-11-29 15:03:52 +0100

    [+] Series Info Model [~] Series Detail Page [~] Search Series Page

---
+++ AniWorldReminder_Webpanel/Models/EpisodeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AniWorldReminder_Webpanel.Models
+{
+    public class EpisodeModel
+    {
+        public int Id { get; set; }
+        public int SeriesId { get; set; }
+        public int Season { get; set; }
+        public int Episode { get; set; }
+        public string? Name { get; set; }
+        public Language Languages { get; set; }
+    }
+}
---
+++ AniWorldReminder_Webpanel/Models/SeasonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AniWorldReminder_Webpanel.Models
+{
+    public class SeasonModel
+    {
+        public int Id { get; set; }
+        public int EpisodeCount { get; set; }
+        public List<EpisodeModel> Episodes { get; set; } = new List<EpisodeModel>();
+    }
+}
