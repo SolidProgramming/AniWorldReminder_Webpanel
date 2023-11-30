476be1831472dbba3a4f63fd4c4a08270108f627
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2023-11-30 17:49:08 +0100

    [+] Get Users Series Info [+] SeriesModel [+] StreamingPortalModel

---
+++ AniWorldReminder_Webpanel/Models/SeriesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AniWorldReminder_Webpanel.Models
+{
+    public class SeriesModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? CoverArtUrl { get; set; }
+        public StreamingPortalModel? StreamingPortal { get; set; }
+    }
+}
---
+++ AniWorldReminder_Webpanel/Models/StreamingPortalModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AniWorldReminder_Webpanel.Models
+{
+    public class StreamingPortalModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
