65c1a0071d6945d4fedde00fec7c1fc5576df196
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2023-11-30 21:01:58 +0100

    [+] Description [+] Direct Link

--- AniWorldReminder_Webpanel/Classes/SeriesInfoModel.cs
+++ AniWorldReminder_Webpanel/Classes/SeriesInfoModel.cs
@@ -3,8 +3,10 @@
     public class SeriesInfoModel
     {
         public string? Name { get; set; }
+        public string? Description { get; set; }
         public int SeasonCount { get; set; }
         public string? CoverArtUrl { get; set; }
+        public string? DirectLink { get; set; }
         public List<SeasonModel> Seasons { get; set; } = new List<SeasonModel>();
     }
 }
