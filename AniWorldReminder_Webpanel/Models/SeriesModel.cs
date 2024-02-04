69994bc6681dc483f2114278b61f89a099f3ae6c
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-02-04 23:42:24 +0100

    [~] Card styling [~] Dashboard behavior

--- AniWorldReminder_Webpanel/Models/SeriesModel.cs
+++ AniWorldReminder_Webpanel/Models/SeriesModel.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? CoverArtUrl { get; set; }
-        public StreamingPortalModel? StreamingPortal { get; set; }
+        public StreamingPortal StreamingPortal { get; set; }
         public Language LanguageFlag { get; set; }
+        public bool IsSearchResult = false;
     }
 }
