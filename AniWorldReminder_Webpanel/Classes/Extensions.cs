9f9a456e8989c0038244cd8ba7d77bf6827ad2de
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-02-20 13:09:02 +0100

    [+] Add Downloads

--- AniWorldReminder_Webpanel/Classes/Extensions.cs
+++ AniWorldReminder_Webpanel/Classes/Extensions.cs
@@ -11,5 +11,14 @@
                 .Replace(",", "")
                 .Replace("’", "");
         }
+        public static IEnumerable<T> GetFlags<T>(this Enum input, Enum? ignore = default)
+        {
+            foreach (T value in Enum.GetValues(input.GetType()))
+            {
+                Enum? enumVal = (Enum)Convert.ChangeType(value, typeof(Enum));
+                if (!enumVal.Equals(ignore) && input.HasFlag(enumVal))
+                    yield return value;
+            }
+        }
     }
 }
---
+++ AniWorldReminder_Webpanel/Models/AddDownloadsRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AniWorldReminder_Webpanel.Models
+{
+    public class AddDownloadsRequestModel
+    {
+        public string? SeriesId { get; set; }
+        public List<EpisodeModel>? Episodes { get; set; }
+    }
+}
