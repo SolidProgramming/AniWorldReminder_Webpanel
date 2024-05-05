e0468f84e2b5d28ebbe0a4750084e4502e41d23c
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-05-05 18:30:53 +0200

    [+] MegaKino changes

--- AniWorldReminder_Webpanel/Classes/Routes.cs
+++ AniWorldReminder_Webpanel/Classes/Routes.cs
@@ -7,7 +7,7 @@
         public const string Login = "/login";
         public const string Logout = "/logout";
         public const string Verify = "/verify";
-        public const string SeriesDetail = "/series";
+        public const string MediaDetail = "/details";
         public const string Dashboard = "/dashboard";
         public const string Settings = "/settings";
         public const string CreateDownloader = "/createDownloader";
